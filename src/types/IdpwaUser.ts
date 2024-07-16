2e1225a208ec2e90d7168d7929972c921cfba67b
abhirampjayan <abhiramp08@gmail.com>
2024-07-17 00:33:43 +0530

    Created UserModel

--- src/types/IdpwaUser.ts
+++ src/types/IdpwaUser.ts
@@ -1,27 +1,35 @@
-import { User } from "firebase/auth";
+// import { User } from "firebase/auth";
 
-export interface IdpwaUser extends User{
-  uid: string;
-  phoneNumber: string;
-  alternatePhoneNumber?: string;
-  isNewUser: boolean;
-  name?: string;
-  age?: number;
-  firmName?: string;
-  firmAddress?: {
-    address: string;
-    city: string;
-    state: string;
-    pincode: string;
-  };
-  HomeAddress?: {
-    address: string;
-    city: string;
-    state: string;
-    pincode: string;
-  };
-  taluk?: string;
-  district?: string;
-  state?: string;
-  CO?: string;
+// export interface IdpwaUser extends User{
+//   uid: string;
+//   phoneNumber: string;
+//   alternatePhoneNumber?: string;
+//   isNewUser: boolean;
+//   name?: string;
+//   age?: number;
+//   firmName?: string;
+//   firmAddress?: {
+//     address: string;
+//     city: string;
+//     state: string;
+//     pincode: string;
+//   };
+//   HomeAddress?: {
+//     address: string;
+//     city: string;
+//     state: string;
+//     pincode: string;
+//   };
+//   taluk?: string;
+//   district?: string;
+//   state?: string;
+//   CO?: string;
+// }
+
+export interface Address {
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: string;
+  pincode: string;
 }
