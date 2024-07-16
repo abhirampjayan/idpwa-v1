fc829be7dcf878fdf8fa27d1a9f6542508cd368c
abhirampjayan <abhiramp08@gmail.com>
2024-07-17 01:01:37 +0530

    auth middleware added

---
+++ src/server/middlewares/authMiddleware.ts
@@ -0,0 +1,15 @@
+// server/middlewares/authMiddleware.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+
+export async function authMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => Promise<void>
+) {
+  const session = await getSession({ req });
+  if (!session) return res.status(401).json({ error: 'Unauthorized' });
+
+  await next();
+}
