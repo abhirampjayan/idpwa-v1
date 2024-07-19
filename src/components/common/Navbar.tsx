05c17e0159ce9d4e82cb848ba73b22f191e41703
abhirampjayan <abhiramp08@gmail.com>
2024-07-19 19:53:09 +0530

    creATED REGISTER PAGE

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import IDPWALogo from './IDPWALogo';
+import { Button } from '../ui/button';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className='bg-slate-800 flex'>
+      <div className='container flex justify-between items-center'>
+        <div className='p-4'>
+          <Link href='/'>
+            <div className='flex items-center gap-4'>
+              <IDPWALogo />
+              <h1 className='text-4xl font-semibold text-gray-100'>IDPWA</h1>
+            </div>
+          </Link>
+        </div>
+        <div className='flex'>
+          <Link href='/'>
+            <Button className='bg-blue-500 hover:bg-blue-600'>Sign In</Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/register.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/common/Navbar';
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div className='flex flex-col bg-slate-50 min-h-screen '>
+      <Navbar />
+    </div>
+  );
+};
+
+export default SignUp;
