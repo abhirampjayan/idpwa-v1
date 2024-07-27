98ad168c4fdb0dd3c0614bbe5ee642c2a930568a
jincy-dev <jincydev3007@gmail.com>
2024-07-27 22:42:52 +0530

    added blood group

--- src/components/UserRegForm.tsx
+++ src/components/UserRegForm.tsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import { Card } from './ui/card';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
-import { Button } from './ui/button';
-import { Checkbox } from './ui/checkbox';
-import Link from 'next/link';
-import DatePicker from './DatePicker';
-import SelectGender from './SelectGender';
+import React from "react";
+import { Card } from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import Link from "next/link";
+import DatePicker from "./DatePicker";
+import SelectGender from "./SelectGender";
+import SelectBloodGroup from "./SelectBloodGroup";
 
 type Props = {};
 
 const UserRegForm = (props: Props) => {
   return (
-    <div className='flex flex-col gap-8 mb-12'>
-      <div className='flex flex-col gap-4 '>
-        <h2 className='text-2xl'>Personal Details</h2>
-        <Card className='grid p-8 grid-cols-3 gap-4'>
+    <div className="flex flex-col gap-8 mb-12">
+      <div className="flex flex-col gap-4 ">
+        <h2 className="text-2xl">Personal Details</h2>
+        <Card className="grid p-8 grid-cols-3 gap-4">
           <div>
             <Label>Full name</Label>
             <Input />
           </div>
           <div>
             <Label>Email(Optional)</Label>
-            <Input type='email' />
+            <Input type="email" />
           </div>
           <div>
             <Label>Phone Number</Label>
-            <Input type='number' maxLength={10} />
+            <Input type="number" maxLength={10} />
           </div>
           <div>
             <Label>WhatsApp Number</Label>
-            <Input type='number' maxLength={10} />
+            <Input type="number" maxLength={10} />
           </div>
           <div>
             <Label>Date of Birth</Label>
@@ -40,7 +41,6 @@ const UserRegForm = (props: Props) => {
           </div>
           <div>
             <Label>Gender</Label>
-
             <SelectGender />
           </div>
           <div>
@@ -55,11 +55,15 @@ const UserRegForm = (props: Props) => {
             <Label>Spouse(Optional)</Label>
             <Input />
           </div>
+          <div>
+            <Label>Blood Group</Label>
+            <SelectBloodGroup />
+          </div>
         </Card>
       </div>
-      <div className='flex flex-col gap-4 '>
-        <h2 className='text-2xl'>Firm Details</h2>
-        <Card className='grid p-8 grid-cols-3 gap-4'>
+      <div className="flex flex-col gap-4 ">
+        <h2 className="text-2xl">Firm Details</h2>
+        <Card className="grid p-8 grid-cols-3 gap-4">
           <div>
             <Label>Firm name</Label>
             <Input />
@@ -94,21 +98,21 @@ const UserRegForm = (props: Props) => {
           </div>
           <div>
             <Label>Phone Number</Label>
-            <Input type='number' maxLength={10} />
+            <Input type="number" maxLength={10} />
           </div>
           <div>
             <Label>Alternate Phone Number(Optional)</Label>
-            <Input type='number' maxLength={10} />
+            <Input type="number" maxLength={10} />
           </div>
           <div>
             <Label>Email(Optional)</Label>
-            <Input type='email' />
+            <Input type="email" />
           </div>
         </Card>
       </div>
-      <div className='flex flex-col gap-4 '>
-        <h2 className='text-2xl'>Home Address</h2>
-        <Card className='grid p-8 grid-cols-3 gap-4'>
+      <div className="flex flex-col gap-4 ">
+        <h2 className="text-2xl">Home Address</h2>
+        <Card className="grid p-8 grid-cols-3 gap-4">
           <div>
             <Label>Address Line 1</Label>
             <Input />
@@ -139,26 +143,26 @@ const UserRegForm = (props: Props) => {
           </div>
         </Card>
       </div>
-      <div className=''>
-        <div className='flex gap-4 text-slate-500'>
-          <Checkbox id='terms' />
+      <div className="">
+        <div className="flex gap-4 text-slate-500">
+          <Checkbox id="terms" />
           <label
-            htmlFor='terms'
-            className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+            htmlFor="terms"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            I agree to the{' '}
+            I agree to the{" "}
             <Link
-              href='/terms-and-conditions'
-              target='_blank'
-              className='text-blue-600'
+              href="/terms-and-conditions"
+              target="_blank"
+              className="text-blue-600"
             >
               Terms and Conditions
-            </Link>{' '}
-            and{' '}
+            </Link>{" "}
+            and{" "}
             <Link
-              href='/privacy-policy'
-              target='_blank'
-              className='text-blue-600'
+              href="/privacy-policy"
+              target="_blank"
+              className="text-blue-600"
             >
               Privacy Policy
             </Link>
@@ -168,7 +172,7 @@ const UserRegForm = (props: Props) => {
           </label>
         </div>
       </div>
-      <div className='flex justify-end'>
+      <div className="flex justify-end">
         <Button>Confirm & Register</Button>
       </div>
     </div>
