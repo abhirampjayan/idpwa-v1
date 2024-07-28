f0c50a71440d356738098f926797c4018ed7a954
abhirampjayan <abhiramp08@gmail.com>
2024-07-28 23:44:53 +0530

    made responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,8 +34,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between'>
-      <div className='grid grid-cols-2 w-screen grow'>
-        <div className='relative'>
+      <div className='grid md:grid-cols-2 w-screen grow'>
+        <div className='hidden md:flex relative'>
           <Image
             src='/idpwa.png'
             alt='IDPWA'
@@ -43,7 +43,7 @@ export default function Home() {
             className='object-cover'
           />
         </div>
-        <div className='flex flex-col justify-center items-center gap-6'>
+        <div className='flex flex-col justify-center items-center gap-8 p-8'>
           <h1 className='text-4xl capitalize font-bold'>Sign In</h1>
           <SignIn />
         </div>
